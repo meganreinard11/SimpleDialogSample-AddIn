4bad4273c9efd1e1dd0ce20656026fcec3929161
meganreinard11 <35755033+meganreinard11@users.noreply.github.com>
2025-10-07 19:40:20 -0400

    Create taskpane.js

---
+++ taskpane/taskpane.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var messageBanner;
+
+// The initialize function must be defined each time a new page is loaded.
+Office.initialize = function (reason) {
+    $(document).ready(function () {
+        // Initialize the FabricUI notification mechanism and hide it
+        var element = document.querySelector('.ms-MessageBanner');
+        messageBanner = new app.notification.MessageBanner(element);
+        messageBanner.hideBanner();
+        if (!Office.context.requirements.isSetSupported('WordApi', '1.1')) {
+            $('#subtitle').text("Opps!");
+            $("#template-description").text("Sorry, this sample requires Word 2016 or later. The button will not open a dialog.");
+            $('#button-text').text("Button");
+            $('#button-desc').text("Button that opens dialog only on Word 2016 or later.");
+            return;
+        }
+        $('#button-text').text("Pick a number (pop-up)");
+        $('#button-desc').text("Pick your favorite number");
+        $('#action-button').click(openDialog);
+        $('#button-text2').text("Pick a number (IFrame**)");
+        $('#button-desc2').text("Pick your favorite number");
+        $('#action-button2').click(openDialogAsIframe);
+    });
+};
+
+function errorHandler(error) {
+    showNotification(error);
+}
+
+function showNotification(content) {
+    $("#notificationBody").text(content);
+    messageBanner.showBanner();
+    messageBanner.toggleExpansion();
+}
